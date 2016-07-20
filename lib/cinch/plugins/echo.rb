a643a87abaa0ea590cf79938896ec78b7fefe084
s0ph0s-2 <alexander.g.langley@gmail.com>
2016-07-20 15:33:39 -0400

    Adding documentation.

--- lib/cinch/plugins/echo.rb
+++ lib/cinch/plugins/echo.rb
@@ -3,8 +3,23 @@ module Cinch
     class Echo
       include Cinch::Plugin
 
-      match /say\s+(.*)/i, :method => :command_say # !say I am a bot
-      match /do\s+(.*)/i, :method => :command_do # !do cuddles
+      match /say\s+(.*)\s(.*)/i, :method => :command_say # !say I am a bot
+      match /do\s+(.*)\s(.*)/i, :method => :command_do # !do cuddles
+
+      def help
+        "!say — Echo back the arguments in a given channel or to a user."
+        "!do — Echo back the arguments in a given channel or to a user, as an ACTION message."
+      end
+
+      def help_say
+        "!say — Echo back the arguments in a given channel or to a user."
+        "Usage: !say #example Hello!"
+      end
+
+      def help_do
+        "!do — Echo back the arguments in a given channel or to a user, as an ACTION message."
+        "Usage: !do #example wags."
+      end
 
       def command_say(m, text)
         m.reply text
