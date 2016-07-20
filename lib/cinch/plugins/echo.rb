f2c0611b4bdb92023c706f059ff8bc5212e7ff92
s0ph0s-2 <alexander.g.langley@gmail.com>
2016-07-20 17:03:58 -0400

    Fleshing out the echo plugin a bit.

--- lib/cinch/plugins/echo.rb
+++ lib/cinch/plugins/echo.rb
@@ -3,30 +3,49 @@ module Cinch
     class Echo
       include Cinch::Plugin
 
-      match /say\s+(.*)\s(.*)/i, :method => :command_say # !say I am a bot
-      match /do\s+(.*)\s(.*)/i, :method => :command_do # !do cuddles
+      # Per RFC 2812, nicks are [A-z_\-\[\]\\^{}|`][A-z0-9_\-\[\]\\^{}|`]{0,8}
+      #               chans are [#&][^\x07\x2C\s]{,200}
+      # My particular server is configured to permit nicks up to 40 chars.
+      match /say\s+([A-z_\-\[\]\\^{}|`][A-z0-9_\-\[\]\\^{}|`]{1,39}|[#&][^\x07\x2C\s]{,200})\s+(.*)/i, :method => :command_say # !say #channel I am a bot
+      match /do\s+([A-z_\-\[\]\\^{}|`][A-z0-9_\-\[\]\\^{}|`]{1,39}|[#&][^\x07\x2C\s]{,200})\s+(.*)/i, :method => :command_do # !do ExampleUser cuddles
 
       def help
-        "!say — Echo back the arguments in a given channel or to a user."
-        "!do — Echo back the arguments in a given channel or to a user, as an ACTION message."
+        [
+          "!say — Echo back the arguments in a given channel or to a user.",
+          "!do — Echo back the arguments in a given channel or to a user, as an ACTION message."
+        ].join("\n")
       end
 
       def help_say
-        "!say — Echo back the arguments in a given channel or to a user."
-        "Usage: !say #example Hello!"
+        [
+          "!say — Echo back the arguments in a given channel or to a user.",
+          "Usage: !say #example Hello!"
+        ].join("\n")
       end
 
       def help_do
-        "!do — Echo back the arguments in a given channel or to a user, as an ACTION message."
-        "Usage: !do #example wags."
+        [
+          "!do — Echo back the arguments in a given channel or to a user, as an ACTION message.",
+          "Usage: !do #example wags."
+        ].join("\n")
       end
 
-      def command_say(m, text)
-        m.reply text
+      def command_say(m, dest, msg)
+        if dest[1] == "#"
+          Channel(dest).send(msg)
+        else
+          User(dest).send(msg)
+        end
       end
 
-      def command_do(m, action)
-        m.action_reply action
+      def command_do(m, dest, action)
+        if dest[1] == "#"
+          Channel(dest).action(action)
+        else
+          User(dest).action(action)
+        end
+      end
+
     end
   end
 end
