de40e3e88a4a27d20c9fe600f4b37eda3915d1dd
s0ph0s-2 <alexander.g.langley@gmail.com>
2016-09-17 20:59:12 -0400

    Nope, the config file was wrong.

--- lib/models/quotelist.rb
+++ lib/models/quotelist.rb
@@ -17,7 +17,7 @@ class QuoteList
   def quote_for(name)
     canonical_name = canonicalize name
     return '' if !canonical_name
-    @quotes[canonical_name][:quotes].shuffle.sample
+    @quotes[canonical_name][:quotes].sample
   end
 
   def add(name, quote)
